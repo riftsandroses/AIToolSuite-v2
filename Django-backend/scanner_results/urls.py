72c0333242b1722159bee6aa512679b2d17c2adb
Utkarsh <utkarsh.cpp@gmail.com>
2025-04-27 22:48:20 +0530

    Added unencrypted zip file APIs

--- Django-backend/scanner_results/urls.py
+++ Django-backend/scanner_results/urls.py
@@ -8,4 +8,8 @@ router.register(r'results', ScanResultsViewSet, basename='results')
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Add explicit path for the new endpoint
+    path('download-unencrypted-zip/', 
+         ScanResultsViewSet.as_view({'get': 'download_unencrypted_zip'}), 
+         name='download-unencrypted-zip'),
 ]
